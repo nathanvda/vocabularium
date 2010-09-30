16e8f0b5ab766cd1bdfc81fb654cebc4dbf7add9
nathanvda <nathan@dixis.com>
2010-09-30 20:32:51 +0200

    made sure that a user can edit her name and username (and also when registering)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   has_many :packets
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :fullname
 
   def show_name
     name || fullname || email
