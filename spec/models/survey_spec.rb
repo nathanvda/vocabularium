636e78780a2c0db19bd657bd43e8af24a6dd19ea
nathanvda <nathan@dixis.com>
2010-10-20 22:50:24 +0200

    started survey spec

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe Survey do
+  before (:each) do
+    @survey = Factory(:survey)
+  end
 
-#  it {should validate_presence_of(:name)}
-#  it {should validate_uniqueness_of(:name)}
-#  it {should have_many(:coverages)}
-#  it {should have_one(:courier_configuration)}
+  it {should validate_presence_of(:name)}
+  it {should validate_uniqueness_of(:name)}
+  it {should have_many(:packets)}
+  it {should have_many(:survey_packets)}
+  it {should belong_to(:user)}
 
 end
